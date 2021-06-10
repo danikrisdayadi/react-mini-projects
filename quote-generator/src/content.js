9d6266e2f3e172a7f8648214c1c5a482ce7c12fd
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-06-10 17:46:43 +0800

    Add quotes and authors

---
+++ quote-generator/src/content.js
@@ -0,0 +1,28 @@
+export const QUOTES = 
+[
+    {
+        id: 0,
+        quote: "To be or not to be that is the question",
+        author: "William Shakespeare"
+    },
+    {
+        id: 1,
+        quote: "The purpose of our lives is to be happy",
+        author: "Dalai Lama"
+    },
+    {
+        id: 2,
+        quote: "Life is what happens when you’re busy making other plans",
+        author: "John Lennon"
+    },
+    {
+        id: 3,
+        quote: "Many of life’s failures are people who did not realize how close they were to success when they gave up",
+        author: "Thomas A. Edison"
+    },
+    {
+        id: 4,
+        quote: "Not how long, but how well you have lived is the main thing",
+        author: "Seneca"
+    }
+]
