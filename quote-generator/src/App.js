c3eed673078b00caaf96d9d1237bf6d681b84ad6
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-06-10 17:33:24 +0800

    Add styling and padding

--- quote-generator/src/App.js
+++ quote-generator/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-
+import { Button } from 'react-bootstrap'
 function App() {
   return (
    <div id="wrapper">
     <div id="quote-box">
-      <h1>Test123123</h1>
+      <h1 id="text">To be or not to be, that is the questions</h1>
+      <p id="author">- Someone wise</p>
+      <div id="footer">
+        <a href="/" id="tweet-quote">Tweet</a>
+        <Button id="new-quote">New Quote</Button>
+      </div>
     </div>
    </div>
   );
