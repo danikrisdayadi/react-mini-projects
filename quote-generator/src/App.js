8becc4d958a2d6b4010d6dbf209ae948da9b6026
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-06-11 16:07:01 +0800

    Add icon to center

--- quote-generator/src/App.js
+++ quote-generator/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import {useState} from 'react'
 import { Button } from 'react-bootstrap'
 import {QUOTES as quote}  from './content'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faTwitter} from '@fortawesome/free-brands-svg-icons'
 
 function App() {
   const [quoteID, setQuoteID] = useState(0);
@@ -10,11 +12,11 @@ function App() {
   }
   return (
    <div id="wrapper" style={{backgroundColor: quote[quoteID].colour}}>
-    <div id="quote-box">
+    <div id="quote-box" className="shadow-lg">
       <h1 id="text">{quote[quoteID].quote}</h1>
       <p id="author">- {quote[quoteID].author}</p>
       <div id="footer">
-        <a href="/" id="tweet-quote">Tweet</a>
+        <a href="/" id="tweet-quote"><FontAwesomeIcon size="lg" icon={faTwitter} /></a>
         <Button id="new-quote" onClick={handleClick}>New Quote</Button>
       </div>
     </div>
