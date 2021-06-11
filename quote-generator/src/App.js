20fe1b7059959f96b6251002afad52ec246d7196
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-06-11 14:22:42 +0800

    Add styling and background colour change

--- quote-generator/src/App.js
+++ quote-generator/src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
+import {useState} from 'react'
 import { Button } from 'react-bootstrap'
+import {QUOTES as quote}  from './content'
+
 function App() {
+  const [quoteID, setQuoteID] = useState(0);
+  const [quoteColour, setQuoteColour] = useState(quote[quoteID].colour)
+  const handleClick = () => {
+    setQuoteID(Math.floor(Math.random() * 5));
+  }
   return (
-   <div id="wrapper">
+   <div id="wrapper" style={{backgroundColor: quote[quoteID].colour}}>
     <div id="quote-box">
-      <h1 id="text">To be or not to be, that is the questions</h1>
-      <p id="author">- Someone wise</p>
+      <h1 id="text">{quote[quoteID].quote}</h1>
+      <p id="author">- {quote[quoteID].author}</p>
       <div id="footer">
         <a href="/" id="tweet-quote">Tweet</a>
-        <Button id="new-quote">New Quote</Button>
+        <Button id="new-quote" onClick={handleClick}>New Quote</Button>
       </div>
     </div>
    </div>
--- quote-generator/src/content.js
+++ quote-generator/src/content.js
@@ -3,26 +3,31 @@ export const QUOTES =
     {
         id: 0,
         quote: "To be or not to be that is the question",
-        author: "William Shakespeare"
+        author: "William Shakespeare",
+        colour: "#f94144"
     },
     {
         id: 1,
         quote: "The purpose of our lives is to be happy",
-        author: "Dalai Lama"
+        author: "Dalai Lama",
+        colour: "#f3722c"
     },
     {
         id: 2,
         quote: "Life is what happens when you’re busy making other plans",
-        author: "John Lennon"
+        author: "John Lennon",
+        colour: "#f8961e"
     },
     {
         id: 3,
         quote: "Many of life’s failures are people who did not realize how close they were to success when they gave up",
-        author: "Thomas A. Edison"
+        author: "Thomas A. Edison",
+        colour: "#f9844a"
     },
     {
         id: 4,
         quote: "Not how long, but how well you have lived is the main thing",
-        author: "Seneca"
+        author: "Seneca",
+        colour: "#f9c74f"
     }
 ]
