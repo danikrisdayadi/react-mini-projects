ce64752cdbf33458ce8d270dc21aec2925b1d531
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-06-15 15:09:13 +0800

    Added timer functionality

--- podomoro-timer/src/App.js
+++ podomoro-timer/src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [hours, setHours] = useState(1)
+  const [minutes, setMinutes] = useState(1)
+  const [seconds, setSeconds ] = useState(0)
+  useEffect(() => {
+    if(seconds > 0){
+      setTimeout(() => setSeconds(seconds - 1), 1000);
+    } else if (seconds === 0 && minutes !== 0) {
+      setTimeout(() => {
+        setSeconds(59)
+        setMinutes(minutes - 1)
+      }, 1000);
+    } else if (seconds === 0 && minutes === 0 && hours > 0) {
+        setTimeout(() => {
+        setSeconds(59)
+        setMinutes(59)
+        setHours(hours - 1)
+      }, 1000);
+    }
+  }, [hours, minutes, seconds])
   return (
-    <div>
+    <div className="App">
       <h1>Podomoro Timer</h1>
-      <p>00:00:00</p>
+      <h2>{hours} : {minutes} : {seconds}</h2>
+      {/* <Button>Reset</Button> */}
     </div>
   );
 }
