be93cc983be9c9823d664fd50642ac0d9f7c9c2f
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-06-16 00:02:48 +0800

    Add reset button

--- podomoro-timer/src/App.js
+++ podomoro-timer/src/App.js
@@ -3,14 +3,21 @@ import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
-  const [startTimer, setStartTimer] = useState(false)
-  const [buttonColour, setButtonColour] = useState({
-    colour: "blue",
-    state: "Start"
-  })
-  const [hours, setHours] = useState(1)
-  const [minutes, setMinutes] = useState(1)
-  const [seconds, setSeconds ] = useState(0)
+  const initialState = {
+    startTimer: false,
+    buttonColour: {
+      colour: "blue",
+      state: "Start"
+    },
+    hours: 0,
+    minutes: 25,
+    seconds: 0
+  }
+  const [startTimer, setStartTimer] = useState(initialState.startTimer)
+  const [buttonColour, setButtonColour] = useState(initialState.buttonColour)
+  const [hours, setHours] = useState(initialState.hours)
+  const [minutes, setMinutes] = useState(initialState.minutes)
+  const [seconds, setSeconds ] = useState(initialState.seconds)
   let secTimer;
   useEffect(() => {
     if(startTimer) {
@@ -55,6 +62,14 @@ function App() {
     setStartTimer(!startTimer)
     buttonColour.state === "Start" ? setButtonColour({colour: "red", state: "Pause"}) : setButtonColour({colour: "blue", state: "Start"});
   }
+
+  const resetState = () => {
+    setStartTimer(initialState.startTimer)
+    setButtonColour(initialState.buttonColour)
+    setHours(initialState.hours)
+    setMinutes(initialState.minutes)
+    setSeconds(initialState.seconds)
+  }
   return (
     <div className="App">
       <h1>Podomoro Timer</h1>
@@ -64,6 +79,7 @@ function App() {
       <Button onClick={changeTime} name="increaseMinute" disabled={buttonColour.state === "Pause"}>Increase Minute</Button>
       <Button onClick={changeTime} name="decreaseMinute" disabled={buttonColour.state === "Pause"}>Decrease Minute</Button>
       <Button style={{backgroundColor: buttonColour.colour, borderColor: buttonColour.colour}} onClick={changeState} active>{buttonColour.state} Timer</Button>
+      <Button onClick={resetState} variant="danger" disabled={buttonColour.state === "Pause"}>Reset</Button>
     </div>
   );
 }
