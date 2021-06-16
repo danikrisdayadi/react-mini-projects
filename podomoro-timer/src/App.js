511b937fcba1a17d4fbf8f78c8277fb76377ec33
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-06-16 15:03:55 +0800

    Add break time

--- podomoro-timer/src/App.js
+++ podomoro-timer/src/App.js
@@ -10,9 +10,24 @@ function App() {
       state: "Start"
     },
     hours: 0,
-    minutes: 25,
-    seconds: 0
+    minutes: 1,
+    seconds: 0,
+    isRunning: false
   }
+  const breakState = {
+    startTimer: false,
+    buttonColour: {
+      colour: "blue",
+      state: "Start"
+    },
+    hours: 0,
+    minutes: 2,
+    seconds: 0,
+    isRunning: false
+  }
+  const [numberOfRounds, setnumberOfRounds] = useState(0)
+  const [activityType, setactivityType] = useState("Study Time")
+  const [isRunning, setisRunning] = useState(initialState.isRunning)
   const [startTimer, setStartTimer] = useState(initialState.startTimer)
   const [buttonColour, setButtonColour] = useState(initialState.buttonColour)
   const [hours, setHours] = useState(initialState.hours)
@@ -25,18 +40,34 @@ function App() {
     if(startTimer) {
       if(seconds > 0){
         secTimer = setTimeout(() => setSeconds(seconds - 1), 1000);
-        console.log("time countdown", seconds, startTimer)
       } else if (seconds === 0 && minutes > 0) {
         setTimeout(() => {
-          setSeconds(59)
+          setSeconds(4)
           setMinutes(minutes - 1)
         }, 1000);
       } else if (seconds === 0 && minutes === 0 && hours > 0) {
-          setTimeout(() => {
+        setTimeout(() => {
           setSeconds(59)
           setMinutes(59)
           setHours(hours - 1)
         }, 1000);
+      } else if (seconds === 0 && minutes === 0 && hours === 0) {
+        setisRunning(false); // clock has stopped
+        if (activityType === "Study Time") {
+          changeState()
+          setisRunning(false)
+          setactivityType("Break Time") 
+          setStartTimer(breakState.startTimer)
+          setSeconds(breakState.seconds)
+          setMinutes(breakState.minutes)
+          setHours(breakState.hours)
+          setButtonColour(breakState.buttonColour)
+        } else {
+          resetState()
+          setactivityType("Study Time") 
+          setnumberOfRounds(numberOfRounds - 1);
+        }
+        
       }
     }
     
@@ -62,25 +93,30 @@ function App() {
   const changeState = () => {
     clearTimeout(secTimer);
     setStartTimer(!startTimer)
+    setisRunning(true)
     buttonColour.state === "Start" ? setButtonColour({colour: "red", state: "Pause"}) : setButtonColour({colour: "blue", state: "Start"});
   }
 
   const resetState = () => {
+    setactivityType("Study Time")
     setStartTimer(initialState.startTimer)
     setButtonColour(initialState.buttonColour)
     setHours(initialState.hours)
     setMinutes(initialState.minutes)
     setSeconds(initialState.seconds)
+    setisRunning(initialState.isRunning)
   }
 
   return (
     <div className="App">
       <h1>Podomoro Timer</h1>
+      <br />
+      <h2>{activityType}</h2>
       <h2>{hours < 10 ? "0" : ""}{hours} : {minutes < 10 ? "0" : ""}{minutes} : {seconds < 10 ? "0" : ""}{seconds}</h2>
-      <Button onClick={changeTime} name="increaseHour" disabled={buttonColour.state === "Pause"}>Increase Hour</Button>
-      <Button onClick={changeTime} name="decreaseHour" disabled={buttonColour.state === "Pause"}>Decrease Hour</Button>
-      <Button onClick={changeTime} name="increaseMinute" disabled={buttonColour.state === "Pause"}>Increase Minute</Button>
-      <Button onClick={changeTime} name="decreaseMinute" disabled={buttonColour.state === "Pause"}>Decrease Minute</Button>
+      <Button onClick={changeTime} name="increaseHour" disabled={buttonColour.state === "Pause" || isRunning}>Increase Hour</Button>
+      <Button onClick={changeTime} name="decreaseHour" disabled={buttonColour.state === "Pause" || isRunning}>Decrease Hour</Button>
+      <Button onClick={changeTime} name="increaseMinute" disabled={buttonColour.state === "Pause" || isRunning}>Increase Minute</Button>
+      <Button onClick={changeTime} name="decreaseMinute" disabled={buttonColour.state === "Pause" || isRunning}>Decrease Minute</Button>
       <br />
       <br />
       <Button style={{backgroundColor: buttonColour.colour, borderColor: buttonColour.colour}} onClick={changeState} active>{buttonColour.state} Timer</Button>
