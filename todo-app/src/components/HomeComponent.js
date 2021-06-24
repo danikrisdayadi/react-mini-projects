0df57e98746a84387a4b8a09fb2226e58302102a
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-06-24 16:20:28 +0800

    Present list in ul form

--- todo-app/src/components/HomeComponent.js
+++ todo-app/src/components/HomeComponent.js
@@ -2,41 +2,44 @@ import React, { useState } from 'react';
 
 import {Form, Button, Col} from 'react-bootstrap'
 
+const initialTask = {
+    name: '',
+    status: ''
+}
+
 function Home() {
-    const initialTask = {
-        name: '',
-        status: ''
-    }
-    
-    const [taskList, setTaskList] = useState([{initialTask}])
+    const [taskList, setTaskList] = useState([])
     const [task, setTask] = useState(initialTask)
     const handleChange = (e) => {
         setTask({name: e.target.value, status: "incomplete"})
     }
     const handleSubmit = () => {
-        setTaskList(taskList.push(task))
+        taskList.push(task)
+        setTaskList(taskList)
         setTask(initialTask)
-        taskList.map((tasks) => {
-            console.log(tasks)
-            return null
-        })
     }
+
     return(
         <div>
-            <h1>{task.name}</h1>
+            <h1>Todo List Practise</h1>
             <Form>
                 <Form.Row>
                     <Col xs="auto">
                         <Form.Label srOnly>Add Task</Form.Label>
                     </Col>
                     <Col xs="2">
-                        <Form.Control type="text" placeholder="Enter new task" onChange={handleChange}/>
+                        <Form.Control value={task.name} type="text" placeholder="Enter new task" onChange={handleChange}/>
                     </Col>
                     <Col>
-                        <Button type="submit" onClick={handleSubmit}>Submit</Button>
+                        <Button onClick={handleSubmit}>Submit</Button>
                     </Col>
                 </Form.Row>
             </Form>
+            <ul>
+                {taskList.map( (taskItem, index) => {
+                    return(<li key={index}>{taskItem.name}</li>)
+                })}
+            </ul>
         </div>
         
     )
